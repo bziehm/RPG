93338ea11196692a6ce733f945b3cbfdd5f27e75
bziehm <bziehm@albany.edu>
2021-04-11 23:19:45 -0400

    added else and return statements

--- RPG/conditional.py
+++ RPG/conditional.py
@@ -3,5 +3,7 @@ userinput = input("enter data:")
 def inpfunction(userinput):
     if userinput.lower() == "q" or userinput.lower() == "quit":
         return ("something arbitrary, ha")
+    else:
+        return ("invalid input"
 
     
