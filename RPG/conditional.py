0919d2cc1a98d82a9dcbc89e6ed7f358aab21bec
bziehm <bziehm@albany.edu>
2021-04-11 22:29:13 -0400

    added elif statement

--- RPG/conditional.py
+++ RPG/conditional.py
@@ -2,4 +2,7 @@ userinput = input("enter data:")
 userinput = userinput.lower()
 if userinput == "q":
     print ("something arbitrary, ha")
+elif userinput == "quit" or "Quit" or "QUIT":
+    print ("something more arbitrary")
+
     
