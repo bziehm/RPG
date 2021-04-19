98bae225ccab6359db6ee37f675e46f3a5e3ce41
bziehm <bziehm@albany.edu>
2021-04-18 23:30:50 -0400

    created function and placed code inside

--- RPG/iterationloop.py
+++ RPG/iterationloop.py
@@ -1,7 +1,8 @@
 userinput = input("<")
 userinput = True
 b = false
-if userinput.lower() == "q" or userinput.lower() == "quit":
+def iterloop(userinput):
+    if userinput.lower() == "q" or userinput.lower() == "quit":
     return userinput
 else:
     return b
