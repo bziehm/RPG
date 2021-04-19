96b1122b20f42e11e2a6e80cc4b37a14e95fa438
bziehm <bziehm@albany.edu>
2021-04-18 23:04:03 -0400

    added 'a' variable after input

--- RPG/conditional.py
+++ RPG/conditional.py
@@ -1,15 +1,11 @@
 userinput = input("enter data:")
-
-
+a= "something arbitrary"
 def function1(userinput):
-    a="test"
-    b="fail"
+    
     if userinput.lower() == "q" or userinput.lower() == "quit":
         return a
     else:
         return b
-
-function1(userinput)
 
 
 
