d75111788a631b0752220c815cb762b232e2fbf6
bziehm <bziehm@albany.edu>
2021-04-18 21:31:14 -0400

    updated descriptions

---
+++ RPG/rpgrooms/dictionaryforrooms.py
@@ -0,0 +1,19 @@
+
+
+data = {"key":"value"}
+rooms_data = {
+    "Prison Cell": "Description 1",
+    "Hallway": "Description 2",
+    "guards": "Description 3",
+    "unlocked cell": "description 4",
+    "room behind toilet": "Description 5",
+    "exit to freedom": "Description 6"}
+    
+import csv
+directions="directions.csv"
+ 
+DictionaryRooms()
+with open (directions, 'r') as data:
+    
+    for line in csv.DictReader(data):
+        print(line)
