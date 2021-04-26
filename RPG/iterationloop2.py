6567d7427ff2f46f8fe9c60d41c631076d1ac0ac
bziehm <bziehm@albany.edu>
2021-04-25 23:42:38 -0400

    changed operator from != to ==

--- RPG/iterationloop2.py
+++ RPG/iterationloop2.py
@@ -4,7 +4,7 @@ def iterloop(userinput):
     userinput=""
     while True:
         userinput=input("type here")
-        if userinput.lower() != "q" or userinput.lower() != "quit":
+        if userinput.lower() == "q" or userinput.lower() == "quit":
             print ("goodbye")
             break
 
