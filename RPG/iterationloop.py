47e9e787fdfa21eb3449988419745b9a16207a91
bziehm <bziehm@albany.edu>
2021-04-25 21:44:03 -0400

    put if statement instide while loop

--- RPG/iterationloop.py
+++ RPG/iterationloop.py
@@ -6,12 +6,12 @@ userinput = input("Enter Data<")
 def iterloop(userinput):
     result=0
     print("enter q or quit to exit")
-    if userinput.lower() == "q" or userinput.lower() == "quit":
+    while True:
+        
+        if userinput.lower() == "q" or userinput.lower() == "quit":
         userinput = False
     else:
         userinput = True
-
-while True:
     userinput
 else:
     print("end program")
