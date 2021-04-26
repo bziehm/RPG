6d06a20899a75bc03067177822f5bb2f1c49d455
bziehm <bziehm@albany.edu>
2021-04-25 23:25:22 -0400

    removed a and b return statments, program stopped terminating

--- RPG/iterationloop2.py
+++ RPG/iterationloop2.py
@@ -1,15 +1,14 @@
 userinput=input("type here>")
-b =userinput
-a = quit()
+
 def iterloop(userinput):
     
     while True:
         if userinput.lower() != "q" or userinput.lower() != "quit":
             print ("goodbye")
             break
-test=iterloop(userinput)
 
-print(test)
+
+print(userinput)
         
 
             
