853b6285dc5642d9c47d74229ae37e1cfac2cc55
bziehm <bziehm@albany.edu>
2021-04-25 23:17:16 -0400

    made it while true

--- RPG/iterationloop2.py
+++ RPG/iterationloop2.py
@@ -1,3 +1,4 @@
+userinput = True
 userinput=input("type here>")
 b =userinput
 a = quit()
@@ -5,12 +6,8 @@ def iterloop(userinput):
     userinput = ''
     while True:
         if userinput.lower() != "q" or userinput.lower() != "quit":
-            userinput=False
-        else:
-            userinput=True
+            print ("goodbye")
+        
 
-call=iterloop(userinput)
-print(call)
-            
             
                     
