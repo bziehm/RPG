ec2ca6536bc249838e01f8557d6080e19ba252e3
bziehm <bziehm@albany.edu>
2021-04-25 23:22:30 -0400

    attempted to call function by setting it to 'test'

--- RPG/iterationloop2.py
+++ RPG/iterationloop2.py
@@ -1,12 +1,15 @@
-userinput = True
 userinput=input("type here>")
 b =userinput
 a = quit()
 def iterloop(userinput):
-    userinput = ''
+    
     while True:
         if userinput.lower() != "q" or userinput.lower() != "quit":
             print ("goodbye")
+            break
+test=iterloop(userinput)
+
+print(test)
         
 
             
