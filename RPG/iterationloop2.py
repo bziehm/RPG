4846686f5bd7e49ab4c1ccad79276763cd949fa2
bziehm <bziehm@albany.edu>
2021-04-25 23:29:26 -0400

    added a second userinput, this one inside while loop

--- RPG/iterationloop2.py
+++ RPG/iterationloop2.py
@@ -3,6 +3,7 @@ userinput=input("type here>")
 def iterloop(userinput):
     
     while True:
+        userinput=input("type here")
         if userinput.lower() != "q" or userinput.lower() != "quit":
             print ("goodbye")
             break
