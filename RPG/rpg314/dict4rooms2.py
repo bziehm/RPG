d9e95f118150f2ef31681620913b7d441f7e4529
bziehm <bziehm@albany.edu>
2021-05-02 18:02:12 -0400

    began combining my iteration loop program with dictionary program

---
+++ RPG/rpg314/dict4rooms2.py
@@ -0,0 +1,26 @@
+data = {"key":"value"}
+rooms_data = {
+    "Prison Cell": "A cold bare cell, A bed with no sheets lays in the corner,",
+    "Hallway": "A narrow hallway with a guard room to the East and another prison cell to the west.",
+    "guards": "Guard Room watching the Exit.",
+    "unlocked cell": "An empty cell with a bed in one corner and a toilet in the North wall.",
+    "room behind toilet": "Appears to be a hole behind the toilet that leads to a small plumbing room.",
+    "exit to freedom": "There is an exit to the north through the plumbing pipes. "}
+print (rooms_data['Hallway'])
+
+userinput=""
+
+def iterloop(userinput):
+    
+    while True:
+        userinput=input("type here")
+        if userinput.lower() == "q" or userinput.lower() == "quit":
+            print ("goodbye")
+            break
+        else:
+            userinput
+
+call=iterloop(userinput)
+print (call)
+
+            
