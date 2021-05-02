abac2a82e4f506f0cd4517a7cc99e46ce31bfecb
bziehm <bziehm@albany.edu>
2021-05-02 17:58:36 -0400

    updated descriptions for rooms

--- RPG/rpg314/dictionaryforrooms.py
+++ RPG/rpg314/dictionaryforrooms.py
@@ -1,9 +1,9 @@
 data = {"key":"value"}
 rooms_data = {
-    "Prison Cell": "Description 1",
-    "Hallway": "Description 2",
-    "guards": "Description 3",
-    "unlocked cell": "description 4",
-    "room behind toilet": "Description 5",
-    "exit to freedom": "Description 6"}
+    "Prison Cell": "A cold bare cell, A bed with no sheets lays in the corner,",
+    "Hallway": "A narrow hallway with a guard room to the East and another prison cell to the west.",
+    "guards": "Guard Room watching the Exit.",
+    "unlocked cell": "An empty cell with a bed in one corner and a toilet in the North wall.",
+    "room behind toilet": "Appears to be a hole behind the toilet that leads to a small plumbing room.",
+    "exit to freedom": "There is an exit to the north through the plumbing pipes. "}
 print (rooms_data['Hallway'])
